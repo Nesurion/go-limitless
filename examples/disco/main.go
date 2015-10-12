a0c3c4dcae98bcad472b6768a59243be987a19e5
Nesurion <maik.schwan@gmail.com>
2015-10-12 15:32:11 +0200

    adds disco example

---
+++ examples/disco/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/nesurion/go-limitless"
+)
+
+func main() {
+	c := limitless.LimitlessController{}
+	c.Host = "192.168.2.141"
+	group := limitless.LimitlessGroup{}
+	group.Id = 1
+	group.Controller = &c
+	c.Groups = []limitless.LimitlessGroup{group}
+
+	group.Disco()
+	time.Sleep(1000 * time.Millisecond)
+	group.DiscoSlower()
+	time.Sleep(1000 * time.Millisecond)
+	group.DiscoFaster()
+}
